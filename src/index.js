71950fc765294e22af5fdbfac5caccb05a8ea075
Yash Awasthi <godaezakmi@gmail.com>
2025-02-09 03:13:53 +0530

    removed vercel analytics

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import App from './App'
 import { persistor, store } from './redux/store';
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
-import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.render(
     <React.StrictMode>
     <Provider store= {store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
-      <Analytics />
     </PersistGate>
     </Provider>
   </React.StrictMode>
